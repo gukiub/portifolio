8110e3de85cfb254f0daadd60c6872c9f292a068
gukiub <gu_kiub@hotmail.com>
2021-08-26 13:30:11 -0300

    bottomsheetdialog para adicionar um produto

---
+++ white-label-firebase-tutorial/app/src/main/java/br/com/gukiub/whitelabeltutorial/ui/products/addproducts/AddProductViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.gukiub.whitelabeltutorial.ui.products.addproducts
+
+import androidx.lifecycle.ViewModel
+
+class AddProductViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
